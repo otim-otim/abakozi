c7f6c62b70b0696126783241e17962993eb9d205
otim <isaacomega16@gmail.com>
2022-02-20 11:23:49 +0300

    created migrattions for companies and employees

---
+++ database/migrations/2022_02_19_203305_create_employees_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('FirstName',255);
+            $table->string('LastName',255);
+            $table->integer('company')->unsigned();
+            $table->string('email',255)->nullable();
+            $table->string('phone',255)->nullable();
+            $table->foreign('company')->references('id')->on('companies')
+                    ->onUpdate('cascade')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
