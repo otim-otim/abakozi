30f65a214de369dd3b4ff6d34120411722086d81
otim <isaacomega16@gmail.com>
2022-02-20 23:49:35 +0300

    created api routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\CompanyController;
+use App\Http\Controllers\EmployeeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,16 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/company/create',[CompanyController::class,'create']);
+Route::get('/companies',[CompanyController::class,'index']);
+Route::get('/company/{id}',[CompanyController::class,'getCompany']);
+Route::put('/company/{id}',[CompanyController::class,'editCompany']);
+Route::delete('/company/{id}',[CompanyController::class,'deleteCompany']);
+
+Route::post('/employee/create',[EmployeeController::class,'create']);
+Route::get('/employees',[EmployeeController::class,'index']);
+Route::get('/employee/{id}',[EmployeeController::class,'getEmployee']);
+Route::get('/company/{com}/employees/',[EmployeeController::class,'comEmployees']);
+Route::put('/employee/{id}/edit',[EmployeeController::class,'edit']);
+Route::delete('/employee/{id}/delete',[EmployeeController::class,'remove']);
