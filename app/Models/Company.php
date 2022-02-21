04a9b2da824d0b2d0da6c319bb1fcb78e509cb9e
otim <isaacomega16@gmail.com>
2022-02-21 23:16:05 +0300

    laravel auth scafolding

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Company extends Model
 {
     use HasFactory;
+    protected $guarded = [];
 
     public function employees() {
         return $this->hasMany(Employee::class,'company');
--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Employee extends Model
 {
     use HasFactory;
-    
+    protected $fillable = ['FirstName','LastName','email','phone'];
     public function company() {
         return $this->belongsTo(Company::class,'company');
     }
